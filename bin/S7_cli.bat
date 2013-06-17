80c2ba555fd8d85b3a2cc290f0f41f23068a0ec0
Marcin Zapolski <mzapolsk@cern.ch>
2013-06-17 14:38:36 +0000

    CRG-207 Added Makefile-like .bat for building project from scratch to compiled sources.
    git-svn-id: https://svn.cern.ch/reps/lhctccs/Logic_Phase_III/tools/automation/s7cli@3340 bf839a8c-926d-0410-8da8-9396dbd2fcf4

--- bin/S7_cli.bat
+++ bin/S7_cli.bat
@@ -2,43 +2,101 @@
 :: defining variables which will be later used for executing S7 CLI
 
 @echo off
-set newProjectName=new_proj
-set newProjectDirPath=D:\MDudek\Projects\S7_CLI\tests
-set existingProjectDirPath=D:\MDudek\Projects\S7_CLI\tests\new_proj
-set projectConfigPath=D:\MDudek\Projects\S7_CLI\tests\CFP_SHC4_ARC45.CFG
-set symbolsPath=D:\MDudek\Projects\S7_CLI\tests\Symbol.sdf
-set S7ProgramName=S7_prog_name
-set libProjectName=ucpc_plc_siemens_6_1
+set S7CLI=\\cern.ch\dfs\Users\m\mzapolsk\Documents\training\S7CLI\s7cli.exe
+set projectRoot=\\cern.ch\dfs\Users\m\mzapolsk\Documents\training
+set newProjectName=KnightRider
+set newProjectDirPath=%projectRoot%\%newProjectName%
+set existingProjectDirPath=%newProjectDirPath%\KnightRi
+set projectConfigPath=%projectRoot%\simatic300.CFG
+set symbolsPath=%newProjectDirPath%\Output\S7InstanceGenerator\Symbol.sdf
+set S7ProgramName="S7 Program(1)"
+set libProjectName=ucpc_plc_siemens_6_3
 set libProjectProgramName=BASELINE_S7
 set destinationProjectProgramName="S7 Program(1)"
 
-@echo on
+if ""%1""=="""" goto :all
+goto %1
+
+call :all
+goto :eof
+
+:all
+call :createProject
+call :importConfig
+call :importSymbols
+call :importLibSources
+call :importLibBlocks
+call :importSources
+call :compile
+goto :eof
+
+:createProject
 :: ----------------------------------------------------------------------
 :: creating new project
 
-s7_cli.exe createProject -n %newProjectName% -d %newProjectDirPath%
+@echo on
+%S7CLI% createProject --projname %newProjectName% --projdir %newProjectDirPath%
+@echo off
+goto :eof
 
+:importConfig
 :: ----------------------------------------------------------------------
 :: importing config to existing project
 
-s7_cli.exe importProjectConfig -d %existingProjectDirPath% -c %projectConfigPath%
+@echo on
+%S7CLI% importConfig -p %existingProjectDirPath% -c %projectConfigPath%
+@echo off
+goto :eof
 
+:importSymbols
 :: ----------------------------------------------------------------------
 :: importing symbols to existing project
 
-s7_cli.exe importSymbols -d %existingProjectDirPath% -s %symbolsPath%
+:: %S7CLI% importSymbols -p %existingProjectDirPath% -s %symbolsPath%
 
 :: ----------------------------------------------------------------------
 :: importing symbols to existing project with the program name different that "S7 Program(1)"
 
-::s7_cli.exe importSymbols -d %existingProjectDirPath% -s %symbolsPath% -p %S7ProgramName%
+@echo on
+%S7CLI% importSymbols -p %existingProjectDirPath% -s %symbolsPath% --program %S7ProgramName%
+@echo off
+goto :eof
 
+:importLibSources
 :: ----------------------------------------------------------------------
 :: importing sources from library
 
-s7_cli.exe importLibSources -d %existingProjectDirPath% --lib-proj-name %libProjectName% --lib-proj-prog-name %libProjectProgramName% --dest-proj-prog-name %destinationProjectProgramName%
+@echo on
+%S7CLI% importLibSources --project %existingProjectDirPath% --program %destinationProjectProgramName% --library %libProjectName% --libprg %libProjectProgramName% 
+@echo off
+goto :eof
 
+:importLibBlocks
 :: ----------------------------------------------------------------------
 :: importing blocks from library
 
-s7_cli.exe importLibBlocks -d %existingProjectDirPath% --lib-proj-name %libProjectName% --lib-proj-prog-name %libProjectProgramName% --dest-proj-prog-name %destinationProjectProgramName%
+@echo on
+%S7CLI% importLibBlocks --project %existingProjectDirPath% --program %destinationProjectProgramName% --library %libProjectName% --libprg %libProjectProgramName% 
+@echo off
+goto :eof
+
+:importSources
+:: ----------------------------------------------------------------------
+:: importing instance and logic sources
+
+@echo on
+%S7CLI% importSourcesDir --project %existingProjectDirPath% --program %destinationProjectProgramName% --srcdir %newProjectDirPath%\Output\S7InstanceGenerator 
+%S7CLI% importSourcesDir --project %existingProjectDirPath% --program %destinationProjectProgramName% --srcdir %newProjectDirPath%\Output\S7LogicGenerator 
+@echo off
+
+goto :eof
+
+:compile
+:: ----------------------------------------------------------------------
+:: compiling sources
+
+@echo on
+%S7CLI% compileSources --project %existingProjectDirPath% --program %destinationProjectProgramName% --sources 1_Compilation_Baseline,2_Compilation_instance,3_Compilation_LOGIC,4_Compilation_OB
+@echo off
+
+goto :eof
