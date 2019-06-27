b6759cc0f07520bc314ee6c9f4ebefb6973fb193
Tomasz Wolak <tomasz.wolak@cern.ch>
2019-06-27 11:05:05 +0200

    Adding first tests of option parser, TECRGCE-202

--- UnitTests_s7cli/UnitTest_s7cli.cs
+++ UnitTests_s7cli/UnitTest_s7cli.cs
@@ -16,4 +16,73 @@ namespace UnitTests_s7cli
 
         }
     }
+
+
+    [TestClass]
+    public class UnitTest_OptionParser
+    {
+        [TestMethod]
+        public void TestMethod_constructor()
+        {
+            S7_cli.Option_parser parser = new S7_cli.Option_parser(null);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            string[] args = { };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            args = new string[] { "non-existent-cmd"};
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            args = new string[] { "createProject" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            args = new string[] { "createProject", "--an_incorrect_option" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            // missing option arg
+            args = new string[] { "createProject", "--projname" }; 
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            // missing 2nd option
+            args = new string[] { "createProject", "--projname", "test" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual( false, parser.optionsOK() );
+
+            // incorrect 2nd option
+            args = new string[] { "createProject", "--projname", "test", "--not_an_option" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            // missing 2nd option's arg.
+            args = new string[] { "createProject", "--projname", "test", "--projname" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            // incorrect 1st option
+            args = new string[] { "createProject", "--projname_bad", "test", "--projdir", "test" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            // incorrect 2nd option
+            args = new string[] { "createProject", "--projname", "test", "--projdir_bad", "test" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(false, parser.optionsOK());
+
+            // OK
+            args = new string[] { "createProject", "--projname", "test", "--projdir", "test" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(true, parser.optionsOK());
+
+
+            args = new string[] { "listProjects" };
+            parser = new S7_cli.Option_parser(args);
+            Assert.AreEqual(true, parser.optionsOK());
+
+        }
+    }
 }
