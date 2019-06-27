f38244ed8e0bdec1b58bd67f69b87024ba7f91b4
Tomasz Wolak <tomasz.wolak@cern.ch>
2019-06-27 10:01:01 +0200

    Initial version of unit tests module, TECRGCE-202

---
+++ UnitTests_s7cli/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests_s7cli")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests_s7cli")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a0e98f19-5509-44f0-a18b-f5a779489d3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests_s7cli/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using S7_cli;
+
+namespace UnitTests_s7cli
+//namespace S7_cli
+{
+    [TestClass]
+    public class UnitTest_s7cli
+    {
+        [TestMethod]
+        public void TestMethod_main()
+        {
+            string [] args = { };
+            Assert.AreEqual(0, S7_cli.s7cli.Main(args));
+
+        }
+    }
+}
