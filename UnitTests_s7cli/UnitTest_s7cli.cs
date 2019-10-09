c7c81e069ea472cfc1df66f5da27214135bbb931
Joao Borrego <joao.borrego@cern.ch>
2019-10-09 14:12:10 +0200

    fix unit tests for OptionParser
    Refactored set of tests related to command-line argument parsing
    The OptionParser wrapper does not currently return the parsed arguments, but this can easily be added if it proves important for testing corner cases.
    In any case, relying on third-party CommandLine library should eliminate the need for testing these lower-level problems

--- UnitTests_s7cli/UnitTest_s7cli.cs
+++ UnitTests_s7cli/UnitTest_s7cli.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using S7_cli;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests_s7cli
-//namespace S7_cli
 {
     [TestClass]
     public class UnitTest_s7cli
     {
         [TestMethod]
-        public void TestMethod_main()
+        public void TestMain()
         {
             string [] args = { };
             Assert.AreEqual(0, S7_cli.s7cli.Main(args));
@@ -43,100 +40,48 @@ namespace UnitTests_s7cli
 
 
     [TestClass]
-    public class UnitTest_OptionParser
+    public class OptionParserTests
     {
         [TestMethod]
-        public void TestMethod_constructor()
+        public void TestParseOptions()
         {
-            S7_cli.Option_parser parser = new S7_cli.Option_parser(null);
-            Assert.AreEqual(false, parser.optionsOK());
-            Assert.IsNull( parser.getCommand() );
-
             string[] args = { };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-            Assert.IsNull(parser.getCommand());
-
-            args = new string[] { "non-existent-cmd"};
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-            Assert.IsNull(parser.getCommand());
-
+            // No verb
+            args = new string[] {};
+            Assert.AreEqual(0, S7_cli.OptionParser.parse(args, run: false));
+            // Invalid verb
+            args = new string[] { "invalidVerb" };
+            Assert.AreEqual(1, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, missing required option
             args = new string[] { "createProject" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-            Assert.IsNotNull(parser.getCommand());
-            Assert.AreEqual( "createProject", parser.getCommand() );
-
-            args = new string[] { "createProject", "--an_incorrect_option" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-
-            // missing option arg
-            args = new string[] { "createProject", "--projname" }; 
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-
-            // missing 2nd option
-            args = new string[] { "createProject", "--projname", "test" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual( false, parser.optionsOK() );
-
-            // incorrect 2nd option
-            args = new string[] { "createProject", "--projname", "test", "--not_an_option" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-
-            // missing 2nd option's arg.
-            args = new string[] { "createProject", "--projname", "test", "--projname" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-
-            // incorrect 1st option
-            args = new string[] { "createProject", "--projname_bad", "test", "--projdir", "test" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-
-            // incorrect 2nd option
-            args = new string[] { "createProject", "--projname", "test", "--projdir_bad", "test" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-
-            // empty name
-            args = new string[] { "createProject", "--projname", "", "--projdir", "test" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-
-            // empty dir
-            args = new string[] { "createProject", "--projname", "test", "--projdir", "" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(false, parser.optionsOK());
-
-
-            // OK
-            args = new string[] { "createProject", "--projname", "testprj", "--projdir", "testdir", "--debug", "3" };
-            S7_cli.Logger.setLevel(3);
-            parser = new S7_cli.Option_parser( args );
-            Assert.AreEqual( true, parser.optionsOK() );
-            Assert.AreEqual( "testprj", parser.getOption( "--projname" ) );
-            Assert.AreEqual( "testdir", parser.getOption( "--projdir" ) );
-            //System.Console.WriteLine("debug option:" + )
-            Assert.AreEqual( "3", parser.getOption("--debug") );
-
-            // OK
-            args = new string[] { "createProject", "--projname", "testprj", "--projdir", "testdir", "-d", "3" };
-            S7_cli.Logger.setLevel(3);
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(true, parser.optionsOK());
-            Assert.AreEqual("testprj", parser.getOption("--projname"));
-            Assert.AreEqual("testdir", parser.getOption("--projdir"));
-            Assert.AreEqual("3", parser.getOption("--debug"));
-
-
+            Assert.AreEqual(1, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, unknown option
+            args = new string[] { "createProject", "--incorrect_option"};
+            Assert.AreEqual(1, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, incomplete option
+            args = new string[] { "createProject", "--projname" };
+            Assert.AreEqual(1, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, incorrect option
+            args = new string[] { "createProject", "--projname", "NAME", "--incorrect_option" };
+            Assert.AreEqual(1, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, incomplete option
+            args = new string[] { "createProject", "--projname", "NAME", "--projdir" };
+            Assert.AreEqual(1, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, empty option
+            args = new string[] { "createProject", "--projname", "--projdir" };
+            Assert.AreEqual(1, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, valid options
+            args = new string[] { "createProject", "--projname", "NAME", "--projdir", "DIR" };
+            Assert.AreEqual(0, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, valid options, optional flags
+            args = new string[] { "createProject", "--projname", "NAME", "--projdir", "DIR", "--debug", "3"};
+            Assert.AreEqual(0, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, valid options, optional flags
+            args = new string[] { "createProject", "--projname", "NAME", "--projdir", "DIR", "-d", "3" };
+            Assert.AreEqual(0, S7_cli.OptionParser.parse(args, run: false));
+            // Valid verb, no options needed
             args = new string[] { "listProjects" };
-            parser = new S7_cli.Option_parser(args);
-            Assert.AreEqual(true, parser.optionsOK());
-
+            Assert.AreEqual(0, S7_cli.OptionParser.parse(args, run: false));
         }
     }
 }
