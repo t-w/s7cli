cf455ab18a88157d292a100f1eb5c78d567516bd
Joao Borrego <joao.borrego@cern.ch>
2020-03-02 11:42:12 +0100

    Add C# gRPC Service Server implementation
    Added a Solution for a C# server that can fulfil Step7 requests, thus providing a Step7 service via a gRPC server

---
+++ Server/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Step7Service;
+
+namespace Step7Server
+{
+    class Step7Impl : Step7.Step7Base
+    {
+        public override Task<StatusReply> CreateProject(CreateProjectRequest req, ServerCallContext context)
+        {
+            var command = new S7_cli.S7Command();
+            command.createProject(req.ProjectName, req.ProjectDir);
+            return Task.FromResult(new StatusReply { ExitCode = 0 });
+        }
+
+        public override Task<StatusReply> ImportLibSources(ImportLibRequest req, ServerCallContext context)
+        {
+            var command = new S7_cli.S7Command();
+            command.importLibSources(req.Project, req.LibraryName, req.LibraryProgram, req.Program, req.Force);
+            return Task.FromResult(new StatusReply { ExitCode = 0 });
+        }
+
+        public override Task<StatusReply> ImportLibBlocks(ImportLibRequest req, ServerCallContext context)
+        {
+            var command = new S7_cli.S7Command();
+            command.importLibBlocks(req.Project, req.LibraryName, req.LibraryProgram, req.Program, req.Force);
+            return Task.FromResult(new StatusReply { ExitCode = 0 });
+        }
+
+        public override Task<StatusReply> ImportSymbols(ImportSymbolsRequest req, ServerCallContext context)
+        {
+            var command = new S7_cli.S7Command();
+            command.importSymbols(req.Project, req.Symbols, req.Program);
+            return Task.FromResult(new StatusReply { ExitCode = 0 });
+        }
+
+        public override Task<StatusReply> ImportSourcesDir(ImportSourcesDirRequest req, ServerCallContext context)
+        {
+            var command = new S7_cli.S7Command();
+            command.importSourcesDir(req.Project, req.Program, req.SourcesDir, req.Force);
+            return Task.FromResult(new StatusReply { ExitCode = 0 });
+        }
+
+        public override Task<StatusReply> CompileSources(CompileSourcesRequest req, ServerCallContext context)
+        {
+            string[] sources = new string[] { };
+            req.Sources.CopyTo(sources, 0);
+            var command = new S7_cli.S7Command();
+            command.compileSources(req.Project, req.Program, sources);
+            return Task.FromResult(new StatusReply { ExitCode = 0 });
+        }
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Step7.BindService(new Step7Impl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Step7 server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
