8d4e0e200cc00cb79773895508cfea92a7adbed7
Joao Borrego <joao.borrego@cern.ch>
2020-03-16 15:06:14 +0100

    Add script to generate assembly version
    Wrote Python script that modifies shared assembly information file using environment variables set by a CI

---
+++ scripts/version.py
@@ -0,0 +1,29 @@
+import os
+from argparse import ArgumentParser
+
+
+def main():
+
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename",
+                        help="path to sharedAssemblyInfo.cs file", metavar="FILE")
+    args = parser.parse_args()
+
+    version = "0.0.0.0"
+    if os.environ.get("CI_COMMIT_TAG"):
+        version = os.environ["CI_COMMIT_TAG"]
+    elif os.environ.get("CI_JOB_ID"):
+        version = os.environ["CI_JOB_ID"]
+
+    content = (
+        f'using System.Reflection;\n\n'
+        f'[assembly: AssemblyVersion("{version}")]\n'
+        f'[assembly: AssemblyFileVersion("{version}")]'
+    )
+
+    with open(args.filename,'w') as out:
+        out.write(content)
+
+
+if __name__ == "__main__":
+    main()
