83ba9d510aa23336e4cbe209d6405fccc2b677b9
Joao Borrego <joao.borrego@cern.ch>
2020-03-18 17:49:53 +0100

    Add S7Lib and unit tests
    Added protoytpe for static STEP 7 API library
    Added respective unittests for creating and removing test project

---
+++ S7Lib/S7Lib.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+using SimaticLib;
+using S7HCOM_XLib;
+
+
+namespace S7Lib
+{
+    public static class Api
+    {
+        private static Serilog.Core.Logger CreateLog()
+        {
+             return new LoggerConfiguration().MinimumLevel.Debug()
+                .WriteTo.Console().CreateLogger();            
+        }
+    
+        public static int CreateProject(string projectName, string projectDir)
+        {
+            var api = new Simatic();
+            var log = CreateLog();
+
+            if (projectName.Length > 8)
+            {
+                log.Error($"Could not create project {projectName} in {projectDir}: " +
+                          $"Name can have at most 8 characters");
+                return -1;
+            }
+
+            try
+            {
+                api.Projects.Add(Name: projectName, ProjectRootDir: projectDir);
+            }
+            catch (Exception exc)
+            {
+                log.Error($"Could not create project {projectName} in {projectDir}:", exc);
+                return -1;
+            }
+            return 0;
+        }
+        
+        public static int RemoveProject(string projectName)
+        {
+            var api = new Simatic();
+            var log = CreateLog();
+
+            try
+            {
+                api.Projects.Remove(Index: projectName);
+            }
+            catch (Exception exc)
+            {
+                log.Error($"Could remove create project {projectName}:", exc);
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ UnitTestS7Lib/UnitTestS7Lib.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using S7Lib;
+
+namespace UnitTestS7Lib
+{
+    [TestClass]
+    public class Project
+    {
+        static string workspaceDir = Path.Combine(Path.GetTempPath(), "UnitTestS7");
+
+        [TestMethod]
+        public void TestCreateRemoveProject()
+        {
+            var rv = Api.CreateProject("testProj", workspaceDir);
+            Assert.AreEqual(0, rv);
+            var s7ProjFilePath = Path.Combine(workspaceDir, @"testProj\testProj.s7p");
+            var projectExists = File.Exists(s7ProjFilePath);
+            Assert.IsTrue(projectExists);
+            rv = Api.RemoveProject("testProj");
+            Assert.AreEqual(0, rv);
+            projectExists = File.Exists(s7ProjFilePath);
+            Assert.IsFalse(projectExists);
+        }
+
+        [TestMethod]
+        public void TestCreateInvalidProject()
+        {
+            var rv = Api.CreateProject("testProject", workspaceDir);
+            Assert.AreEqual(-1, rv);
+        }
+
+        [TestMethod]
+        public void TestRemoveInvalidProject()
+        {
+            var rv = Api.RemoveProject("invalid");
+            Assert.AreEqual(-1, rv);
+        }
+    }
+}
