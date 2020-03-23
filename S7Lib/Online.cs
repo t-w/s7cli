ca60c6a1eabb65a321721e88daa8071ffb0664f5
Joao Borrego <joao.borrego@cern.ch>
2020-03-23 12:58:47 +0100

    Add online methods
    Add method to download a block container for a particular program.
    Add private method to retrieve a program from the "path" to their child module.
    Programs must be uniquely specified by their child module reference (not name)

---
+++ S7Lib/Online.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using SimaticLib;
+using S7HCOM_XLib;
+
+
+namespace S7Lib
+{
+    /// <summary>
+    /// Contains methods that require a real connection to a PLC
+    /// </summary>
+    public static class Online
+    {
+        /// <summary>
+        /// Obtain S7 program from its child module
+        /// </summary>
+        /// <remarks>
+        /// The only way to uniquely identify a program is to specify its child module.
+        /// A program name may not be unique.
+        /// To understand the structure of an existing project use SimTree API demo.
+        /// It shows a tree in a GUI and highlights the type of each element in it
+        /// </remarks>
+        /// <param name="project">Project name</param>
+        /// <param name="station">Station name</param>
+        /// <param name="rack">Rack name</param>
+        /// <param name="module">Child module name</param>
+        /// <returns>S7Program on success, null otherwise</returns>
+        private static S7Program getProgram(string project, string station, string rack, string module)
+        {
+            var api = Api.CreateApi();
+            var log = Api.CreateLog();
+
+            IS7Station6 stationObj;
+            try
+            {
+                stationObj = api.Projects[project].Stations[station];
+            }
+            catch (Exception exc)
+            {
+                log.Error(exc, $"Could not find station {station} in project {project}");
+                return null;
+            }
+            IS7Module moduleObj;
+            try
+            {
+                moduleObj = stationObj.Racks[rack].Modules[module];
+            }
+            catch (Exception exc)
+            {
+                log.Error(exc, $"Could not find module {module} in {project}:{station}:{rack}");
+                return null;
+            }
+            S7Program programObj;
+            try
+            {
+                programObj = (S7Program)api.Projects[project].Programs[moduleObj];
+            }
+            catch (Exception exc)
+            {
+                log.Error(exc, $"Could not find program for module {module}");
+                return null;
+            }
+            return programObj;
+        }
+
+        /// <summary>
+        /// Downloads all the blocks under an S7Program
+        /// </summary>
+        /// <param name="project">Project name</param>
+        /// <param name="station">Station name</param>
+        /// <param name="rack">Rack name</param>
+        /// <param name="module">Child module name</param>
+        /// <param name="overwrite">Force overwrite of online blocks</param>
+        /// <returns>0 on success, -1 otherwise</returns>
+        public static int DownloadProgramBlocks(string project, string station,
+            string rack, string module, bool overwrite)
+        {
+            var log = Api.CreateLog();
+
+            S7Program programObj = getProgram(project, station, rack, module);
+            if (programObj == null) return -1;
+
+            var flag = overwrite ? S7OverwriteFlags.S7OverwriteAll : S7OverwriteFlags.S7OverwriteAsk;
+
+            try
+            {
+                var blocks = (S7Container)programObj.Next["Blocks"].Next;
+                blocks.Download(flag);
+            }
+            catch (Exception exc)
+            {
+                log.Error(exc, $"Could not download blocks for {programObj.Name} " +
+                               $"{project}:{station}:{rack}:{module}");
+                return -1;
+            }
+
+            log.Debug($"Downloaded blocks for {programObj.Name} " +
+                      $"{project}:{station}:{rack}:{module}");
+            return 0;
+        }
+    }
+}
