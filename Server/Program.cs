830d70601297ba57ce4e52a47ea201eda4540c7a
Joao Borrego <joao.borrego@cern.ch>
2020-03-27 15:15:19 +0100

    Update Server to use S7Lib and new gRPC proto spec
    Add methods specified in gRPC Step7 .proto
    Server-side (S7Lib) logs are saved to a list of strings and returned in the reply message to the client.
    Fix server infinite loop

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,16 +1,277 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Serilog;
+using Serilog.Sinks.ListOfString;
+
 using Grpc.Core;
 using Step7Service;
 
-// TODO: Partition S7Cli in S7Lib and S7CLi and then remove dependency in S7Cli
-// TODO: Refactor S7CommandStatus so that it;s a regular enum and an attribute in S7Command
+using S7Lib;
+
 
 namespace Step7Server
 {
     class Step7Impl : Step7.Step7Base
     {
-        // TODO: Command implementation
+        private S7Context CreateApiContext(ref List<string> log)
+        {
+            var logger = new LoggerConfiguration().WriteTo.StringList(log).CreateLogger();
+            var ctx = new S7Context(log: logger);
+            return ctx;
+        }
+
+        private StatusReply CreateStatusReply(int rv, ref List<string> log)
+        {
+            var reply = new StatusReply { ExitCode = rv };
+            reply.Log.AddRange(log);
+            return reply;
+        }
+
+        private ListReply CreateListReply(int rv, ref List<string> log, ref List<string> list)
+        {
+            var reply = new ListReply { Status = CreateStatusReply(rv, ref log) };
+            reply.Items.AddRange(list);
+            return reply;
+        }
+
+        // Server commands
+
+        // List commands
+
+        private ListReply ListProjectsImpl(ListProjectsRequest req)
+        {
+            var log = new List<string>();
+            var output = new Dictionary<string, string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ListProjects(ctx, ref output);
+            var projectList = new List<string >(output.Keys);
+            return CreateListReply(rv, ref log, ref projectList);
+        }
+
+        public override Task<ListReply> ListProjects(ListProjectsRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ListProjectsImpl(req));
+        }
+
+        private ListReply ListProgramsImpl(ListProgramsRequest req)
+        {
+            var log = new List<string>();
+            var output = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ListPrograms(ctx, ref output, req.Project);
+            return CreateListReply(rv, ref log, ref output);
+        }
+
+        public override Task<ListReply> ListPrograms(ListProgramsRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ListProgramsImpl(req));
+        }
+
+        private ListReply ListContainersImpl(ListContainersRequest req)
+        {
+            var log = new List<string>();
+            var output = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ListContainers(ctx, ref output, req.Project);
+            return CreateListReply(rv, ref log, ref output);
+        }
+
+        public override Task<ListReply> ListContainers(ListContainersRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ListContainersImpl(req));
+        }
+
+        private ListReply ListStationsImpl(ListStationsRequest req)
+        {
+            var log = new List<string>();
+            var output = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ListStations(ctx, ref output, req.Project);
+            return CreateListReply(rv, ref log, ref output);
+        }
+
+        public override Task<ListReply> ListStations(ListStationsRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ListStationsImpl(req));
+        }
+
+        // Project commands
+
+        private StatusReply CreateProjectImpl(CreateProjectRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.CreateProject(ctx, req.ProjectName, req.ProjectDir);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> CreateProject(CreateProjectRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(CreateProjectImpl(req));
+        }
+
+        private StatusReply CreateLibraryImpl(CreateLibraryRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.CreateLibrary(ctx, req.ProjectName, req.ProjectDir);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> CreateLibrary(CreateLibraryRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(CreateLibraryImpl(req));
+        }
+
+        private StatusReply RegisterProjectImpl(RegisterProjectRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.RegisterProject(ctx, req.ProjectFilePath);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> RegisterProject(RegisterProjectRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(RegisterProjectImpl(req));
+        }
+
+        private StatusReply RemoveProjectImpl(RemoveProjectRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.RemoveProject(ctx, req.Project);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> RemoveProject(RemoveProjectRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(RemoveProjectImpl(req));
+        }
+
+        // Source commands
+
+        private StatusReply ImportSourceImpl(ImportSourceRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ImportSource(ctx, req.Project, req.Program, req.Source, req.Overwrite);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> ImportSource(ImportSourceRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ImportSourceImpl(req));
+        }
+
+        private StatusReply ImportSourcesDirImpl(ImportSourcesDirRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ImportSourcesDir(ctx, req.Project, req.Program, req.SourcesDir, req.Overwrite);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> ImportSourcesDir(ImportSourcesDirRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ImportSourcesDirImpl(req));
+        }
+
+        private StatusReply ExportAllSourcesImpl(ExportAllSourcesRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ExportAllSources(ctx, req.Project, req.Program, req.SourcesDir);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> ExportAllSources(ExportAllSourcesRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ExportAllSourcesImpl(req));
+        }
+
+        private StatusReply ImportLibSourcesImpl(ImportLibSourcesRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ImportLibSources(ctx,
+                library: req.Library, libProgram: req.LibProgram,
+                project: req.Project, projProgram: req.ProjProgram, req.Overwrite);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> ImportLibSources(ImportLibSourcesRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ImportLibSourcesImpl(req));
+        }
+
+        private StatusReply ImportLibBlocksImpl(ImportLibBlocksRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ImportLibBlocks(ctx,
+                library: req.Library, libProgram: req.LibProgram,
+                project: req.Project, projProgram: req.ProjProgram, req.Overwrite);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> ImportLibBlocks(ImportLibBlocksRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ImportLibBlocksImpl(req));
+        }
+
+        private StatusReply ImportSymbolsImpl(ImportSymbolsRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ImportSymbols(ctx, req.Project, req.Program, req.SymbolFile, req.Flag, req.AllowConflicts);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> ImportSymbols(ImportSymbolsRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ImportSymbolsImpl(req));
+        }
+
+        private StatusReply ExportSymbolsImpl(ExportSymbolsRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.ExportSymbols(ctx, req.Project, req.Program, req.SymbolFile);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> ExportSymbols(ExportSymbolsRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(ExportSymbolsImpl(req));
+        }
+
+        private StatusReply CompileSourceImpl(CompileSourceRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.CompileSource(ctx, req.Project, req.Program, req.Source);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> CompileSource(CompileSourceRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(CompileSourceImpl(req));
+        }
+
+        private StatusReply CompileAllStationsImpl(CompileAllStationsRequest req)
+        {
+            var log = new List<string>();
+            var ctx = CreateApiContext(ref log);
+            var rv = Api.CompileAllStations(ctx, req.Project, req.AllowFail);
+            return CreateStatusReply(rv, ref log);
+        }
+
+        public override Task<StatusReply> CompileAllStations(CompileAllStationsRequest req, ServerCallContext context)
+        {
+            return Task.FromResult(CompileAllStationsImpl(req));
+        }
     }
 
     class Program
@@ -27,11 +288,9 @@ namespace Step7Server
             server.Start();
 
             Console.WriteLine("Step7 server listening on port " + Port);
-
-            // TODO: Fix infinite loop
             try
             {
-                while (true) { Console.Read(); }
+                Console.Read();
             }
             catch (Exception exc)
             {
