a8c372fc4039578770a4159e4ea52b6e30c041ea
Joao Borrego <joao.borrego@cern.ch>
2020-03-30 12:08:22 +0200

    Create setup.py for Python gRPC stubs module

---
+++ Service/Python/setup.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+import os
+
+version = "0.0.0.0"
+if os.environ.get("CI_COMMIT_TAG"):
+    version = os.environ["CI_COMMIT_TAG"]
+elif os.environ.get("CI_JOB_ID"):
+    version = os.environ["CI_JOB_ID"]
+
+
+setup(
+    name='s7service',
+    version=version,
+    description='Python gRPC stubs for Step 7 Service',
+    url='',
+    author='João Borrego',
+    author_email='joao.borrego@cern.ch',
+    license='CERN',
+    packages=find_packages(),
+)
