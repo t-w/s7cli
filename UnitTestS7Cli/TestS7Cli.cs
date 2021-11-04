de04176148dd9a23e7aafaf39883e6cf8d950513
Joao Borrego <joao.borrego@cern.ch>
2021-11-04 18:55:04 +0100

    Fix parser return values in test

--- UnitTestS7Cli/TestS7Cli.cs
+++ UnitTestS7Cli/TestS7Cli.cs
@@ -10,31 +10,31 @@ namespace UnitTestS7Cli
         public void TestMain()
         {
             var parser = new OptionParser(run: false);
-            
+
             // No verb
             string[] args = { };
             Assert.AreEqual(0, parser.Parse(args));
             // Invalid verb
-            args = new string [] { "invalidVerb" };
-            Assert.AreEqual(-1, parser.Parse(args));
+            args = new string[] { "invalidVerb" };
+            Assert.AreEqual(1, parser.Parse(args));
             // Valid help flag
             args = new string[] { "--help" };
             Assert.AreEqual(0, parser.Parse(args));
             // Invalid help flag
             args = new string[] { "-h" };
-            Assert.AreEqual(-1, parser.Parse(args));
+            Assert.AreEqual(1, parser.Parse(args));
             // Invalid verb
             args = new string[] { "invalidVerb", "--help" };
-            Assert.AreEqual(-1, parser.Parse(args));
+            Assert.AreEqual(1, parser.Parse(args));
             // Invalid verb, invalid help flag
             args = new string[] { "invalidVerb", "-h" };
-            Assert.AreEqual(-1, parser.Parse(args));
+            Assert.AreEqual(1, parser.Parse(args));
             // Valid verb, valid help flag, missing arguments
             args = new string[] { "createProject", "--help" };
             Assert.AreEqual(0, parser.Parse(args));
             // Valid verb, invalid help flag, missing arguments
             args = new string[] { "createProject", "-h" };
-            Assert.AreEqual(-1, parser.Parse(args));
+            Assert.AreEqual(1, parser.Parse(args));
             // Valid version flag
             args = new string[] { "--version" };
             Assert.AreEqual(0, parser.Parse(args));
