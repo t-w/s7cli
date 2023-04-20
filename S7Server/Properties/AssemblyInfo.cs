0bba39e52560ed6a41193dc9d917a843a00634d8
Joao Borrego <joao.borrego@cern.ch>
2023-04-20 17:56:41 +0200

    Revert "Remove AssemblyInfo.cs files"
    This reverts commit 6a510bc5bee010e158cd70d64ba7701ddfd7caab

---
+++ S7Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("S7Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("S7Server")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cc1df3a6-058c-4ff0-bd45-b7f2242b490a")]
+
+// Version information from SharedAssemblyInfo.cs
